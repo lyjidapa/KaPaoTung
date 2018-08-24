1581d86a21ccd987e6f77b0e6463d82714f1f91c
Jidapa Kongsakoonwong <a_leeliie@hotmail.co.th>
2018-08-24 20:33:30 +0700

    Initial Commit

---
+++ KaPaoTung/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KaPaoTung
+//
+//  Created by Jidapa Kongsakoonwong on 24/8/2561 BE.
+//  Copyright © 2561 Jidapa Kongsakoonwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
